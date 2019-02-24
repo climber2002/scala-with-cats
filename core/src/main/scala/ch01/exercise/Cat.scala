fc1c0c655b0648cccbdb76bc293124d54b638460
Andy Wang <climber2002@gmail.com>
2019-02-25 00:19:04 +1100

    Add Eq to Cat

--- core/src/main/scala/ch01/exercise/Cat.scala
+++ core/src/main/scala/ch01/exercise/Cat.scala
@@ -1,6 +1,10 @@
 package ch01.exercise
 
 import cats.Show
+import cats.Eq
+import cats.instances.int._    // for Eq
+import cats.instances.string._ // for Eq
+import cats.syntax.eq._ 
 
 final case class Cat(name: String, age: Int, color: String)
 
@@ -11,4 +15,10 @@ object Cat {
   }
 
   implicit val catShow: Show[Cat] = Show.show(cat => s"${cat.name} is a ${cat.age} year-old ${cat.color} cat.")
+
+  implicit val catEq: Eq[Cat] = Eq.instance[Cat] { (cat1, cat2) =>
+    cat1.name === cat2.name &&
+    cat1.age === cat2.age &&
+    cat1.color === cat2.color
+  }
 }
--- tests/src/test/scala/ch01/exercise/CatSpec.scala
+++ tests/src/test/scala/ch01/exercise/CatSpec.scala
@@ -3,6 +3,7 @@ package ch01.exercise
 import org.scalatest._
 
 import cats.Show
+import cats.Eq
 
 class CatSpec extends FlatSpec with Matchers {
   "Printable" should "format cat" in {
@@ -13,5 +14,12 @@ class CatSpec extends FlatSpec with Matchers {
   it should "show cat" in {
     val cat = Cat("Maomao", 2, "gold")
     Show.apply[Cat].show(cat) should be ("Maomao is a 2 year-old gold cat.")
+  }
+
+  it should "compare cats" in {
+    val cat1 = Cat("Maomao", 2, "gold")
+    val cat2 = Cat("Maomao", 2, "gold")
+    
+    (Eq[Cat].eqv(cat1, cat2)) should be (true)
   }
 }
