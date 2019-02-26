16b6262e07951d63cf16862008d40bbc53f6a160
Andy Wang <climber2002@gmail.com>
2019-02-26 20:38:18 +1100

    update

--- core/src/main/scala/ch02/exercise/SuperAdder.scala
+++ core/src/main/scala/ch02/exercise/SuperAdder.scala
@@ -2,9 +2,29 @@ package ch02.exercise
 
 import cats.Monoid
 import cats.instances.int._
+import cats.instances.option._
+
+case class Order(totalCost: Double, quantity: Double)
+
+object Order {
+  implicit val orderMonoid = new Monoid[Order] {
+    override def empty: Order = Order(0, 0)
+
+    override def combine(x: Order, y: Order): Order = Order(x.totalCost + y.totalCost, x.quantity + y.quantity)
+  }
+}
 
 object SuperAdder {
+  def add[A](items: List[A])(implicit monoid: Monoid[A]): A =
+    items.foldLeft(monoid.empty)((item1, item2) => monoid.combine(item1, item2))
+
   def add(items: List[Int]): Int = {
-    Monoid[Int].combineAll(items)
+    add(items)
   }
+
+  def addOption(items: List[Option[Int]]): Option[Int] = {
+    add(items)
+  }
+
+  def addOrders(items: List[Order]): Order = add(items)
 }
--- tests/src/test/scala/ch02/excercise/SuperAdderSpec.scala
+++ tests/src/test/scala/ch02/excercise/SuperAdderSpec.scala
@@ -7,4 +7,8 @@ class SuperAdderSpec extends FlatSpec with Matchers {
   it should "add items" in {
     SuperAdder.add(List(1, 2, 3)) should be (6)
   }
+
+  it should "add option items" in {
+    SuperAdder.addOption(List(Some(1), None, Some(2), None, Some(3))) should be (Some(6))
+  }
 }
